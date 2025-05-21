518ab063dc309295727fb679a05cef40dcb9b0e7
Addison Fowler <fowler.addison13@gmail.com>
2025-05-21 12:01:47 -0500

    oh wow

--- src/html-css/HomePage.jsx
+++ src/html-css/HomePage.jsx
@@ -1,34 +1,59 @@
 import { Link } from 'react-router-dom';
 import Echos from '../assets/Echos.png';
+import BookOne from '../assets/book_1.png'
+import BookTwo from '../assets/book_2.png'
+import BookThree from '../assets/book_3.png'
+import BookFour from '../assets/book_4.png'
+import BookFive from '../assets/book_5.png'
+import BookSix from '../assets/book_6.png'
+import BookSeven from '../assets/book_7.png'
+import BookEight from '../assets/book_8.png'
 import './HomePage.css';
-
 
 export default function PageOne () {
 
 
     return (
+        <>
         <div className="entire">
             <section className="header">
                 <header>
                     <img src={Echos} alt="logo" id="logo"></img>
                     <h2>Echos & Ink</h2>
                 </header>
-                <hr></hr>
             </section>
-            <section className="mainBody">
-                <h1>Echos & Ink</h1>
-                <p>Pair your stories with the perfect soundtrack.</p>
-            </section>
+            <div className="carosel-with-text">
+                <div className="weirdCaroselThing">
+                    <div className="slider" style={ { "--quantity": 8} }>
+                        <div className="item" style={ { "--position": 1} }><img src={BookEight} alt="book cover"/></div>
+                        <div className="item" style={ { "--position": 2} }><img src={BookSeven} alt="book cover"/></div>
+                        <div className="item" style={ { "--position": 3} }><img src={BookSix} alt="book cover"/></div>
+                        <div className="item" style={ { "--position": 4} }><img src={BookFive} alt="book cover"/></div>
+                        <div className="item" style={ { "--position": 5} }><img src={BookFour} alt="book cover"/></div>
+                        <div className="item" style={ { "--position": 6} }><img src={BookThree} alt="book cover"/></div>
+                        <div className="item" style={ { "--position": 7} }><img src={BookOne} alt="book cover"/></div>
+                        <div className="item" style={ { "--position": 8} }><img src={BookTwo} alt="book cover"/></div>
+                    </div>
+                </div>
+                <div className="mainBody">
+                    <h1>Echos & Ink</h1>
+                    <p>Pair your stories with the perfect soundtrack.</p>
+                </div>
+            </div>
             <section className="buttons">
                 <Link to="/search">
-                    <button type="button" id="getStarted" >Get Started</button>
+                    <button type="button" className="getStarted" >Get Started</button>
                 </Link>
                 <Link to="/library">
-                    <button type="button" id="viewLibrary">View Library</button>
+                    <button type="button" className="viewLibrary">View Library</button>
                 </Link>
             </section>
         </div>
+        </>
     );
 }
 
-
+<div className="mainBody">
+                    <h1>Echos & Ink</h1>
+                    <p>Pair your stories with the perfect soundtrack.</p>
+                </div>
